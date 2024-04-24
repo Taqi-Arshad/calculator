b35a7b76761409a6dd93e95df86e13b553ad1fa2
Muhammad Taqi Arshad <160427741+Taqi-Arshad@users.noreply.github.com>
2024-04-24 21:06:17 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer"
+
+
+// let answer= await inquirer.prompt({
+    // name:"num1",
+    // type:"number",
+    // message:"enter your first number"
+// })
+// let answer2= await inquirer.prompt({
+    // name:"num2",
+    // type:"number",
+    // message:"enter your second number"
+
+// })
+
+// let result = answer.num2 / (answer.num1 * answer num1)
+// console.log(result)
+
+
+// let answer=await inquirer.prompt({
+    // name:"num1",
+    // type:"number",
+    // message:"enter your first number"
+// })
